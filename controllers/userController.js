5ee7a3b1ae33fa09a99c2fa074a452900d897116
vishnu-yv <vishnubabu9876@gmail.com>
2020-10-07 15:16:50 +0530

    Login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,29 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
 export const userDetail = (req, res) =>
